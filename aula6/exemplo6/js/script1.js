af76fdf35ede5ca092518e20e063b64271cadcce
sandinojardim <sandino@ppgsc.ufrn.br>
2021-12-07 15:15:30 -0300

    update folder

---
+++ aula6/exemplo6/js/script1.js
@@ -0,0 +1,8 @@
+
+  var yaakovGreeter = {};
+  yaakovGreeter.name = "Yaakov";
+  var greeting = "Hello ";
+  yaakovGreeter.sayHello = function () {
+    console.log(greeting + yaakovGreeter.name);
+  }
+name = "script1";
